50e8be7fa2d925ec2d755628575482d898c2f527
LucasDaSilva96 <dasilvajunior881@gmail.com>
2023-06-21 11:52:20 +0200

    Create the function that changes the svg image on the play button. From play to pause, and pause to play

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 "use strict";
+
+// DOM - Selections
+const play_pause_btn = document.querySelector(".play-icon");
+
+// eventListeners
+// *** This is for changing the svg image on the play_pause_btn
+play_pause_btn.addEventListener("click", function () {
+  if (play_pause_btn.src.includes("play.svg")) {
+    play_pause_btn.src = "./SVG/SVG/pause.svg";
+  } else if (play_pause_btn.src.includes("pause.svg")) {
+    play_pause_btn.src = "./SVG/SVG/play.svg";
+  }
+});
